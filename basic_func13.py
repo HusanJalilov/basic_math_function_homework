65de7ddb00c558e0717d0ddde9dd229d96f69be4
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-24 09:45:35 +0500

    Task 3

--- basic_func13.py
+++ basic_func13.py
@@ -1,8 +1,9 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
+import math
 def main(a):
-    return sqrt(a)
+    return math.sqrt(a)
 # Example:
 # Input: a = 9
 # Output: 3.0
