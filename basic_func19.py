afd9fac5f1ea45e437f945fe6bd9810b94883817
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-24 12:06:23 +0500

    Task 9

--- basic_func19.py
+++ basic_func19.py
@@ -1,7 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
-
+import math 
+def main(a,b):
+    return abs(a-b)
 # Example:
 # Input: a = 4 b = 11
 # Output: 7
