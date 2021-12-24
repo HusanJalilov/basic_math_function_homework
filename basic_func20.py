524ca629c60c5b60619174f6cc4798b4c2b1857a
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-24 12:07:53 +0500

    Task 10

--- basic_func20.py
+++ basic_func20.py
@@ -1,7 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
-
+import math 
+def main(a,b):
+    return a*b
 # Example:
 # Input: a = 5 b = 4
 # Output: 20
