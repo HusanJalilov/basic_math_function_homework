396899ecb104bd254090994fc872c898b1a4be86
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-24 09:48:21 +0500

    Task 4

--- basic_func14.py
+++ basic_func14.py
@@ -1,7 +1,8 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the floor division of a and b.
-
+def main(a,b):
+    return a//b
 # Example:
 # Input: a = 11 b = 2
 # Output: 5
