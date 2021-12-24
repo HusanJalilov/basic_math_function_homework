26c94662c163356a30c067b1e923a2d3223424f8
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-24 11:59:06 +0500

    Task 6

--- basic_func16.py
+++ basic_func16.py
@@ -1,8 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the sum of a and b.
+import math
 def main(a,b):
-    return sum(a,b)
+    return a+b
 # Example:
 # Input: a = 3 b = 2
 # Output: 5
